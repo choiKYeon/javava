5f5d92c613fa1bc0776c3aff0b55db70ee336aa3
ckh <gusrudchl12@gmail.com>
2023-10-16 08:53:51 +0900

    10-16

--- src/main/java/org/example/controller/SystemController.java
+++ src/main/java/org/example/controller/SystemController.java
@@ -1,7 +1,2 @@
-package org.example.controller;
-
-public class SystemController {
-    public void exit(){
-        System.out.printf("시스템을 종료합니다.");
-    }
+package org.example.controller;public class SystemController {
 }
--- src/main/java/org/example/entity/WiseSaying.java
+++ src/main/java/org/example/entity/WiseSaying.java
@@ -1,22 +1,2 @@
-package org.example.entity;
-
-public class WiseSaying {
-    private long id;
-    private String content;
-    private String author;
-
-    public WiseSaying(long id, String content, String author){
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
-    public long getId() {
-        return id;
-    }
-    public String getContent() {
-        return content;
-    }
-    public String getAuthor() {
-        return author;
-    }
+package org.example;public class WiseSaying {
 }
